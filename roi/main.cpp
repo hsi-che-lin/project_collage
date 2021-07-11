50be07f45e2a0b0e1634e78e5b228542690cd2a0
HSI-CHE LIN <hsichelin@gmail.com>
2021-07-11 15:01:13 +0800

    Add another two method
    setTo is not usable in this project (see the note), and concat is about the same speed of the original copyTo method

--- roi/main.cpp
+++ roi/main.cpp
@@ -7,13 +7,17 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <chrono>
 
 using namespace std;
 using namespace cv;
 
+// #define COPYTO
+#define CONCATE
+// #define SETTO
 // #define SIMPLE
 
-#if !defined(SIMPLE)
+#if defined(COPYTO)
 
 int main(void)
 {
@@ -23,6 +27,8 @@ int main(void)
 	string path = "..\\..\\..\\Resources\\screenshots\\";
 	string read_path;
 	int i, j;
+
+	auto start = chrono::steady_clock::now();
 
 	for (i = 0; i < 14; i++) {
 		for (j = 0; j < 32; j++) {
@@ -35,12 +41,85 @@ int main(void)
 
 	imwrite("..\\..\\..\\Resources\\roi.png", img);
 
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << "use the method of copyTo.()\n";
+	cout << chrono::duration <double, milli> (diff).count() <<endl;
+
 	return 0;
 }
 
-#endif
+#elif defined(CONCATE)
 
-#if defined(SIMPLE)
+int main(void)
+{
+	Mat v_unit[32];
+	Mat h_unit[14];
+	Mat unit;
+	Mat unit_cvt;
+	Mat column;
+	Mat img;
+	string path = "..\\..\\..\\Resources\\screenshots\\";
+	string read_path;
+	int i, j;
+
+	auto start = chrono::steady_clock::now();
+
+	for (i = 0; i < 14; i++) {
+		for (j = 0; j < 32; j++) {
+			read_path = path + to_string(i * 32 + j) + ".png";
+			v_unit[j] = imread(read_path);
+		}
+		vconcat(v_unit, 32, h_unit[i]);
+	}
+
+	hconcat(h_unit, 14, img);
+	imwrite("..\\..\\..\\Resources\\roi.png", img);
+
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << "use the method of concat.()\n";
+	cout << chrono::duration <double, milli> (diff).count() << endl;
+
+	return 0;
+}
+
+#elif defined(SETTO)
+// Note: the function setTo() can only assign the scalar value to matrix but
+//       set value of a matrix to that of another matrix. As a result, this 
+//       function cannot be used in my project
+
+int main(void)
+{
+	Mat img(Size(14 * 256, 32 * 144), CV_8UC3, Scalar(255, 0, 255));
+	Mat unit;
+	Mat unit_cvt;
+	string path = "..\\..\\..\\Resources\\screenshots\\";
+	string read_path;
+	int i, j;
+
+	auto start = chrono::steady_clock::now();
+
+	for (i = 0; i < 14; i++) {
+		for (j = 0; j < 32; j++) {
+			read_path = path + to_string(i * 32 + j) + ".png";
+			unit = imread(read_path);
+			unit.convertTo(unit_cvt, CV_8UC3);				// this line is indispensable
+			Mat(img, Rect(i * 256, j * 144, 256, 144)).setTo(Scalar(255, 255, 0));
+		}
+	}
+
+	imwrite("..\\..\\..\\Resources\\roi.png", img);
+
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << "use the method of setTo.()\n";
+	cout << chrono::duration <double, milli> (diff).count() << endl;
+
+	return 0;
+}
+
+#elif defined(SIMPLE)
 
 int main(void)
 {
